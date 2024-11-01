6ae56f92626ec239268eef6e9dc28c5e1cbc1789
Link Campbell <lcampbell@JY666CWH9K.local>
2024-11-01 16:23:26 -0700

    removed commented job code

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 import app.config as config
+import app.frequent_fliers as ff
 
 
 def main():
@@ -11,54 +12,13 @@ def main():
         bool: Returns True if main successfully initialized and scheduled jobs,
               False if not.
     """
-    # # Write UUIDs to blank cells in program plans.
-    # # Job 1 starts with 30 second intervals.
-    # # Job 2 runs once daily at 1am UTC and looks back 7 days
-    # config.scheduler.add_job(uuid.write_uuids_to_sheets,
-    #                          'interval',
-    #                          args=[config.minutes],
-    #                          seconds=30,
-    #                          id="write_uuids_interval")
-
-    # config.scheduler.add_job(uuid.write_uuids_to_sheets,
-    #                          'cron',
-    #                          args=[10080],
-    #                          day='*/1',
-    #                          hour='1',
-    #                          id="write_uuids_cron")
-
-    # # Create new Jira tickets from Smartsheet.
-    # # Job 1 starts with 2 minute intervals.
-    # # Job 2 runs once daily at 1am UTC and looks back 7 days
-    # config.scheduler.add_job(create_jira_tickets.create_tickets,
-    #                          'interval',
-    #                          args=[config.minutes],
-    #                          minutes=2,
-    #                          id="create_jira_interval")
-
-    # config.scheduler.add_job(create_jira_tickets.create_tickets,
-    #                          'cron',
-    #                          args=[10080],
-    #                          day='*/1',
-    #                          hour='1',
-    #                          id="create_jira_cron")
-
-    # # Sync data between the Jira Index Sheet and all sheets in the workspace
-    # # with a Jira Ticket column. By proxy, syncs data with Jira through the
-    # # Index Sheet
-    # # Job 1 starts with 15 second intervals.
-    # # Job 2 runs once daily at 1am UTC and looks back 7 days
-    # config.scheduler.add_job(jira_sync.bidirectional_sync,
-    #                          'interval',
-    #                          args=[config.minutes],
-    #                          seconds=15,
-    #                          id="sync_jira_interval")
-
-    # config.scheduler.add_job(jira_sync.bidirectional_sync,
-    #                          'cron',
-    #                          args=[10080],
-    #                          day='*/1',
-    #                          hour='1',
-    #                          id="sync_jira_cron")
+    config.scheduler.add_job(
+        ff.run,
+        "cron",
+        month="*",  # Any month
+        day="1",  # First day
+        hour="4",  # 4AM
+        id="create_ff_report_cron",
+    )
 
     return True
