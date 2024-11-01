761e6652014582e4f20d7ab87b939207fbb5ca75
Link Campbell <lcampbell@JY666CWH9K.local>
2024-11-01 16:23:03 -0700

    Change logger, add documentation

--- api/google_api.py
+++ api/google_api.py
@@ -1,60 +1,26 @@
-# import csv
-# import json
 import logging
 import os
 import os.path
 import sys
+import base64
 from pathlib import Path
 
 sys.path.append(str(Path(__file__).parent.parent))
-import base64
-# from email.message import EmailMessage
 
-
-# from google.auth.transport.requests import Request
 from google.oauth2 import service_account
-
-# from google.oauth2.credentials import Credentials
-# from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-#  from googleapiclient.http import MediaFileUpload
 
 # Configure logging
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s"
-)
-
-# Create a logs subdirectory if it doesn't exist
-logs_dir = os.path.join(os.path.dirname(__file__), "logs")
-os.makedirs(logs_dir, exist_ok=True)
-
-# Create a file handler for each log level
-debug_handler = logging.FileHandler(
-    os.path.join(logs_dir, "debug.log"), mode="a"
-)
-debug_handler.setLevel(logging.DEBUG)
-info_handler = logging.FileHandler(
-    os.path.join(logs_dir, "info.log"), mode="a"
-)
-info_handler.setLevel(logging.INFO)
-warning_handler = logging.FileHandler(
-    os.path.join(logs_dir, "warning.log"), mode="a"
-)
-warning_handler.setLevel(logging.WARNING)
-error_handler = logging.FileHandler(
-    os.path.join(logs_dir, "error.log"), mode="a"
-)
-error_handler.setLevel(logging.ERROR)
-
-# Add the handlers to the root logger
-logging.getLogger().addHandler(debug_handler)
-logging.getLogger().addHandler(info_handler)
-logging.getLogger().addHandler(warning_handler)
-logging.getLogger().addHandler(error_handler)
+logger = logging.getLogger(__name__)
 
 
 def load_google_credentials():
+    """Loads the service account credentials from the secrets.json file
+
+    Returns:
+        service_account.Credentials: A credentials object that permits API access to Google services.
+    """
     SCOPES = [
         "https://www.googleapis.com/auth/admin.directory.user.readonly",
         "https://www.googleapis.com/auth/gmail.send",
@@ -72,57 +38,27 @@ def load_google_credentials():
     )
 
     creds = creds.with_subject("svc-incidentiq@it.wusd.org")
-    # service = build("gmail", "v1", credentials=creds)
-
-    # if not creds or not creds.valid:
-    #     if creds and creds.expired and creds.refresh_token:
-    #         creds.refresh(Request())
-    #     else:
-    #         flow = InstalledAppFlow.from_client_secrets_file(
-    #             SERVICE_ACCOUNT_FILE, SCOPES
-    #         )
-    #         creds = flow.run_local_server(port=0)
-    #         # Save the credentials for the next run
-    #     with open("token.json", "w") as token:
-    #         token.write(creds.to_json())
-
-    #     try:
-    #         service = build("docs", "v1", credentials=creds)
-    #     except HttpError as err:
-    #         print(err)
     return creds
 
 
-# svc_creds = load_google_credentials()
-# html = "<p>Here's an HTML Table</p><p></p>"
-
-
 def gmail_send_message(svc_creds, message):
-    """Create and insert a draft email.
-     Print the returned draft's message and id.
-     Returns: Draft object, including draft id and message meta data.
+    """Creates and sends an email via a service account
 
-    Load pre-authorized user credentials from the environment.
-    TODO(developer) - See https://developers.google.com/identity
-    for guides on implementing OAuth2 for the application.
+    Args:
+        svc_creds (serviceaccount.Credentials): The service account retrieved from secrets.json
+        message (EmailMessage): The message to send via Gmail
+
+    Returns:
+        dict: A dict object, including draft id and message meta data.
     """
 
     try:
         # create gmail api client
         service = build("gmail", "v1", credentials=svc_creds)
 
-        # message = EmailMessage()
-
-        # message.set_content(html, subtype="html")
-
-        # message["To"] = "lcampbell@wusd.org"
-        # message["From"] = "svc-incidentiq@it.wusd.org"
-        # message["Subject"] = "HTML TABLE"
-
         # encoded message
         encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
 
-        # create_message = {"message": {"raw": encoded_message}}
         create_message = {"raw": encoded_message}
         # pylint: disable=E1101
         send_message = (
@@ -132,16 +68,24 @@ def gmail_send_message(svc_creds, message):
             .execute()
         )
 
-        print(f'Message Id: {send_message["id"]}')
-
     except HttpError as error:
-        print(f"An error occurred: {error}")
+        logging.error(f"An error occurred: {error}")
         send_message = None
 
     return send_message
 
 
 def create_sheet(folder_id, file_name, creds):
+    """Creates a sheet within a given folder or Google Shared Drive
+
+    Args:
+        folder_id (str): The ID of the folder to create the sheet
+        file_name (str): The name of the Sheet
+        creds (serviceaccount.Credentials): The service account retrieved from secrets.json
+
+    Returns:
+        str: The ID of the created file
+    """
     try:
         # create drive api client
         service = build("drive", "v3", credentials=creds)
@@ -152,25 +96,33 @@ def create_sheet(folder_id, file_name, creds):
             "parents": [folder_id],
         }
         # pylint: disable=maybe-no-member
+        # supportsAllDrives=True to create in Google Shared Drives
         file = (
             service.files()
             .create(body=file_metadata, supportsAllDrives=True, fields="id")
             .execute()
         )
-        # print(f'File ID: "{file.get("id")}".')
         return file.get("id")
 
     except HttpError as error:
-        print(f"An error occurred: {error}")
+        logging.error(f"An error occurred: {error}")
         return None
 
 
 def update_values(
     spreadsheet_id, range_name, value_input_option, values, creds
 ):
-    """
-    Creates the batch_update the user has access to.
-    Load pre-authorized user credentials from the environment.
+    """Updates values within a Google spreadsheet
+
+    Args:
+        spreadsheet_id (str): The ID of the spreadhseet
+        range_name (str): The sheet range to update, e.g. Sheet1!A:Z or Sheet1!1:1000
+        value_input_option (str): One of "USER_ENTERED" or "RAW"
+        values (list): An array of values to update in the range
+        creds (serviceaccount.Credentials): The service account retrieved from secrets.json
+
+    Returns:
+        dict: Returns a dictionary of updated cell results.
     """
     # pylint: disable=maybe-no-member
     try:
@@ -191,7 +143,7 @@ def update_values(
         logging.info(f"{result.get('updatedCells')} cells updated.")
         return result
     except HttpError as error:
-        print(f"An error occurred: {error}")
+        logging.error(f"An error occurred: {error}")
         return error
 
 
@@ -199,6 +151,16 @@ def update_values(
 
 
 def update_sheet(spreadsheet_id, creds, body):
+    """Updates a Google Sheet
+
+    Args:
+        spreadsheet_id (str): The ID of the spreadsheet
+        creds (serviceaccount.Credentials): The service account retrieved from secrets.json
+        body (list): The array of data to update the sheet
+
+    Returns:
+        dict: The metadata of the update action.
+    """
     try:
         service = build("sheets", "v4", credentials=creds)
         result = (
@@ -216,6 +178,15 @@ def update_sheet(spreadsheet_id, creds, body):
 
 
 def get_spreadsheet(spreadsheet_id, creds):
+    """Get spreadsheet information from Google
+
+    Args:
+        spreadsheet_id (str): The ID of the spreadsheet
+        creds (serviceaccount.Credentials): Google credentials retrieved via secrets.json
+
+    Returns:
+        dict: The spreadsheet metadata
+    """
     try:
         service = build("sheets", "v4", credentials=creds)
         result = (
