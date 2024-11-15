0d84b949e1b2da2957b942f073e3ae24986e0bf6
Link Campbell <lcampbell@JY666CWH9K.local>
2024-11-15 11:57:11 -0800

    Add typing, logging, try/catch

--- app/app.py
+++ app/app.py
@@ -1,8 +1,18 @@
+import logging
+import os
+from pathlib import Path
+
 import app.config as config
 import app.frequent_fliers as ff
+import app.variables as app_vars
+
+parent_dir = str(Path(__file__).parent.parent)
+cwd = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+log_location = os.path.join(parent_dir, app_vars.log_location)
+logger = logging.getLogger(__name__)
 
 
-def main():
+def main() -> bool:
     """Configures the scheduler to run jobs. Interval jobs are configured with
       initial defaults, but automatically adapt based on run-time per job.
       Cron jobs are scheduled to run daily with a longer lookback to catch
@@ -12,13 +22,17 @@ def main():
         bool: Returns True if main successfully initialized and scheduled jobs,
               False if not.
     """
-    config.scheduler.add_job(
-        ff.run,
-        "cron",
-        month="*",  # Any month
-        day="1",  # First day
-        hour="4",  # 4AM
-        id="create_ff_report_cron",
-    )
+    try:
+        config.scheduler.add_job(
+            ff.run,
+            "cron",
+            month="*",  # Any month
+            day="1",  # First day
+            hour="4",  # 4AM
+            id="create_ff_report_cron",
+        )
 
-    return True
+        return True
+    except Exception as e:
+        logging.ERROR(e)
+        return False
