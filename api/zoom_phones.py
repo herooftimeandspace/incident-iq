556faaf0f9a04f7f1f0c655a8591777b5dc1d79d
Link Campbell <lcampbell@JY666CWH9K.local>
2025-02-05 17:48:13 -0800

    IT-4594: Add functions for getting phone data

--- api/zoom_phones.py
+++ api/zoom_phones.py
@@ -10,7 +10,7 @@ from app import variables as vars
 
 
 def get_auth_token(secrets: dict) -> str:
-    url = f"{vars.zoom_oauth_url}{secrets["AccountId"]}"
+    url = f"{vars.zoom_oauth_url}{secrets['AccountId']}"
     basic_auth_text = (
         secrets["ClientId"] + ":" + secrets["ClientSecret"]
     )
@@ -39,12 +39,17 @@ def get_auth_token(secrets: dict) -> str:
 
 def get_phones(auth: str, device_status: str = "assigned") -> list:
     phones = []
-    page_size = 100
+    # page_size = 100
 
-    url = "https://api.zoom.us/v2/phone/devices"
+    url = vars.zoom_phone_device_url
+    # url = "https://api.zoom.us/v2/phone/devices"
     headers = {"Authorization": f"Bearer {auth}"}
 
-    default_params = {"type": device_status, "page_size": page_size}
+    default_params = vars.zoom_default_params
+    default_params["type"] = device_status
+    default_params.pop("next_page_token", None)
+    logging.debug(default_params)
+    # default_params = {"type": device_status, "page_size": page_size}
     while True:
         response = requests.get(
             url, headers=headers, params=default_params
@@ -54,12 +59,10 @@ def get_phones(auth: str, device_status: str = "assigned") -> list:
         if response.status_code == 200:
             data = response.json()
             logging.debug(
-                f"Page Size: {data["page_size"]} | Total Records: "
-                f"{data["total_records"]} | Phones Len {len(phones)} | "
-                f"NextPageToken {data["next_page_token"]}"
+                f"Page Size: {data['page_size']} | Total Records: "
+                f"{data['total_records']} | Phones Len {len(phones)} | "
+                f"NextPageToken {data['next_page_token']}"
             )
-
-            # print(data["devices"])
             phones.extend(data["devices"])
 
             if len(phones) == data["total_records"]:
@@ -81,3 +84,103 @@ def get_user_by_id(auth: str, id: str) -> dict | None:
         return data
     else:
         return None
+
+
+def get_phone_users(auth: str) -> list:
+    phone_users = []
+    page_size = 100
+
+    url = "https://api.zoom.us/v2/phone/users"
+    headers = {"Authorization": f"Bearer {auth}"}
+
+    default_params = {"page_size": page_size}
+    while True:
+        response = requests.get(
+            url, headers=headers, params=default_params
+        )
+        response.raise_for_status()
+
+        if response.status_code == 200:
+            data = response.json()
+            logging.debug(
+                f"Page Size: {data['page_size']} | Total Records: "
+                f"{data['total_records']} | Phones Len {len(phone_users)} | "
+                f"NextPageToken {data['next_page_token']}"
+            )
+
+            # print(data["devices"])
+            phone_users.extend(data["users"])
+
+            if len(phone_users) == data["total_records"]:
+                break
+            else:
+                default_params["next_page_token"] = data[
+                    "next_page_token"
+                ]
+
+    return phone_users
+
+
+def get_zoom_user(user_id: str, auth: str) -> dict:
+    user = {}
+    url = f"https://api.zoom.us/v2/users/{user_id}"
+    headers = {"Authorization": f"Bearer {auth}"}
+
+    # default_params = {"page_size": page_size}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    if response.status_code == 200:
+        data = response.json()
+        # logging.debug(data)
+
+        user["Name"] = data["first_name"] + " " + data["last_name"]
+        user["Email"] = data["email"]
+        user["Status"] = data["status"]
+
+    return user
+
+
+def get_shared_line_groups(auth: str) -> list:
+    url = vars.zoom_phone_url + "/shared_line_groups"
+    headers = {"Authorization": f"Bearer {auth}"}
+
+    # default_params = {"page_size": page_size}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    if response.status_code == 200:
+        data = response.json()
+        slg = data["shared_line_groups"]
+
+    return slg
+
+
+def get_auto_receptionist(auth: str) -> list:
+    url = vars.zoom_phone_url + "/auto_receptionists"
+    headers = {"Authorization": f"Bearer {auth}"}
+
+    # default_params = {"page_size": page_size}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    if response.status_code == 200:
+        data = response.json()
+        auto_receptionist = data["auto_receptionists"]
+
+    return auto_receptionist
+
+
+def get_call_queues(auth: str) -> list:
+    url = vars.zoom_phone_url + "/call_queues"
+    headers = {"Authorization": f"Bearer {auth}"}
+
+    # default_params = {"page_size": page_size}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    if response.status_code == 200:
+        data = response.json()
+        call_queues = data["call_queues"]
+
+    return call_queues
